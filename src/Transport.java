277bb31339d820c67b00216f51ce13b154a3f9e0
Pronyushkin_qa <kollaydereva@gmail.com>
2022-09-23 12:02:12 +0300

    Сделано

--- src/Transport.java
+++ src/Transport.java
@@ -1,9 +1,8 @@
 public abstract class Transport {
-    public String modelName;
-    public int wheelsCount;
+    private String modelName;
+    private int wheelsCount;
 
     public Transport(String modelName, int wheelsCount) {
-        super();
 
     }
     public abstract void updateTyre();
